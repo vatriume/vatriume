94efd2bfdd3d7ef16617a38065521f4a20a4142f
kirusfg <kirusfg@gmail.com>
2020-05-28 15:51:49 +0500

    Completed ThemeSwitcher component

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
+// Assets and data
 import logo from './logo.svg'
 import './App.css'
 import courses from './courses.json'
 
-import Schedule from './services/schedule/Schedule'
+// Components
 import Sidebar from './common/Sidebar'
 import Typography from './common/Typography'
+import Schedule from './services/schedule/Schedule'
+
+// FontAwesome icons
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
+library.add(faSun, faMoon)
 
 class App extends React.Component {
     constructor() {
--- app/src/common/ThemeSwitcher.js
+++ app/src/common/ThemeSwitcher.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './ThemeSwitcher.css'
+
 
 class ThemeSwitcher extends React.Component {
     constructor(props) {
@@ -7,13 +10,36 @@ class ThemeSwitcher extends React.Component {
         this.state = {
             dark: true
         }
+
+        this.store = typeof localStorage === 'undefined' ? null : localStorage
     }
 
-    toggle = () => this.setState(prev => ({ dark: !prev.dark }))
+    componentDidMount() {
+        if (this.store) this.setState({ dark: this.store.getItem('ThemeSwitcher') || true })
+        document.getElementsByTagName("body")[0].classList.add((this.state.dark) ? 'dark' : 'light')
+    }
+
+    componentDidUpdate() {
+        if (this.store) this.store.setItem('ThemeSwitcher', this.state.dark)
+    }
+
+    toggle = () => {
+        this.setState(prev => ({ dark: !prev.dark }))
+        document.getElementsByTagName("body")[0].classList.replace((this.state.dark) ? 'dark' : 'light', (this.state.dark) ? 'light' : 'dark')
+    }
 
     render() {
         return (
-            <button onClick={this.toggle}>{this.state.dark ? 'On' : 'Off'}</button>
+            <>
+            <button
+                className="ThemeSwitcher"
+                onClick={this.toggle}
+            >
+                { this.state.dark ? 
+                  <FontAwesomeIcon icon="moon" /> : 
+                  <FontAwesomeIcon icon="sun" /> }
+            </button>
+            </>
         )
     }
 }
