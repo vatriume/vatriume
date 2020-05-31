a9a075afebefda257e364d71e3e455f2c49d8e39
kirusfg <kirusfg@gmail.com>
2020-05-31 12:12:35 +0500

    Fixed the indentation

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import React from 'react'
+import React from "react";
 
 // Assets and data
-import logo from './logo.svg'
-import './App.css'
-import courses from './services/schedule/courses.json'
+import logo from "./logo.svg";
+import "./App.css";
+import courses from "./services/schedule/courses.json";
 
 // Components
-import Sidebar from './common/Sidebar'
-import Typography from './common/Typography'
-import Schedule from './services/schedule/Schedule'
+import Sidebar from "./common/Sidebar";
+import Typography from "./common/Typography";
+import Schedule from "./services/schedule/Schedule";
 
 // FontAwesome icons
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
-library.add(faSun, faMoon)
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+library.add(faSun, faMoon);
 
 class App extends React.Component {
-    constructor() {
-        super()
+  constructor() {
+    super();
 
-        this.state = {
-            data: courses.data
-        }
-    }
+    this.state = {
+      data: courses.data,
+    };
+  }
 
-    render() {
-        return (
-            <div className="App">
-                {console.log(this.state.data)}
-                <Sidebar logo={logo} />
-                <Schedule data={this.state.data} />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="App">
+        {console.log(this.state.data)}
+        <Sidebar logo={logo} />
+        <Schedule data={this.state.data} />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
