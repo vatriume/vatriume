cf753215cd13998abd4128cd8f99c7da40bd05d1
kirusfg <kirusfg@gmail.com>
2020-07-12 16:11:57 +0500

    Moved Firebase app configuration and initialization into separate file

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+
+import "firebase/firestore";
+import "firebase/auth";
+
+// Initializing the app with config so that the app could access data
+const firebaseConfig = {
+  apiKey: "AIzaSyBkiiqYOy7eJ0VTXwrtvd8Dmql2B8pUcM4",
+  authDomain: "vatriume-5776b.firebaseapp.com",
+  databaseURL: "https://vatriume-5776b.firebaseio.com",
+  projectId: "vatriume-5776b",
+  storageBucket: "vatriume-5776b.appspot.com",
+  messagingSenderId: "231019453074",
+  appId: "1:231019453074:web:4161ca0f6b8cc5586a644b",
+  measurementId: "G-3W27Y10HZD",
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+export default firebase;
